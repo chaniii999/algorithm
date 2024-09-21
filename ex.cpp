f869acc6e0c98ffff335e7a97577803c5a883f86
chan <psc0729@naver.com>
2024-09-21 10:31:24 +0900

    nm

--- ex.cpp
+++ ex.cpp
@@ -1,18 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-z
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    long long a,b;
+    double a,b;
     cin >> a >> b;
-    long long a1 = a + b;
-    long long a2 = a - b;
-    long long a3 = a * b;
-    cout << a1 << "\n";
-    cout << a2 << "\n";
-    cout << a3 << "\n";
+    double ans = a / b;
+    cout << ans;
 
 }
